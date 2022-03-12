252d352b0e45527c6ba6a393fe6b027a9a6443dd
Sebastian Karlsen <sebastian@karlsen.fr>
2022-03-12 16:29:05 +0100

    Activate support for colored emojis

--- drw.c
+++ drw.c
@@ -140,11 +140,14 @@ xfont_create(Drw *drw, const char *fontname, FcPattern *fontpattern)
 	 * https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=916349
 	 * and lots more all over the internet.
 	 */
+	/* libxft-bgra provides support for color fonts */
+	/*
 	FcBool iscol;
 	if(FcPatternGetBool(xfont->pattern, FC_COLOR, 0, &iscol) == FcResultMatch && iscol) {
 		XftFontClose(drw->dpy, xfont);
 		return NULL;
 	}
+	*/
 
 	font = ecalloc(1, sizeof(Fnt));
 	font->xfont = xfont;
